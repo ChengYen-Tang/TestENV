5fa2ca7afab4a0aaf2ebb49bc196a250f5b66c59
Kenneth <kenneth@gm.nssh.ntpc.edu.tw>
2019-09-24 02:38:31 +0800

    use SubprocVecEnv

--- env.py
+++ env.py
@@ -5,7 +5,7 @@ class TestEnv(gym.Env):
     metadata = {'render.modes': ['human', 'system', 'none']}
 
     def __init__(self):
-        self.action_space = gym.spaces.MultiDiscrete([3])
+        self.action_space = gym.spaces.MultiDiscrete([2])
 
         self.observation_shape = (1, 100)
         self.observation_space = gym.spaces.Box(low=0, high=1, shape=self.observation_shape, dtype=np.float16)
@@ -13,7 +13,10 @@ class TestEnv(gym.Env):
     def reset(self):
         self.counter = 0
         self.action_state = 2
-        self.valid_actions = [1,1,1]
+        self.valid_actions = [1,1]
+        print('reset')
+        
+        return self.state()
 
     def step(self, action):
         if action[0] == 0:
@@ -21,17 +24,18 @@ class TestEnv(gym.Env):
                 print('Invalid actions')
 
             self.action_state = 0
-            self.valid_actions = [0,1,1]
+            self.valid_actions = [0,1]
 
         if action[0] == 1:
             if self.action_state == 1:
                 print('Invalid actions')
 
             self.action_state = 1
-            self.valid_actions = [1,0,1]
+            self.valid_actions = [1,0]
 
         self.counter += 1
         return self.state(), 0, self.finish(), {'valid_actions' : self.valid_actions}
+
 
     def render(self, mode='human'):
         pass
@@ -41,6 +45,6 @@ class TestEnv(gym.Env):
 
     def state(self):
         temp = np.array([*range(100)])
-        return temp/100
-
-
+        # print(temp/100)
+        obs = temp/100
+        return [obs]
--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 from stable_baselines.common.policies import MlpLnLstmPolicy
-from stable_baselines.common.vec_env import  DummyVecEnv
+from stable_baselines.common.vec_env import  SubprocVecEnv
 from stable_baselines import PPO2
 
 from env import TestEnv
 
-Test_env = DummyVecEnv([lambda: TestEnv()])
 
-model = PPO2(MlpLnLstmPolicy, Test_env, verbose=0, nminibatches=1)
-model.learn(total_timesteps = 10000)
+if __name__ == '__main__':
+    Test_env = SubprocVecEnv([lambda: TestEnv() for i in range(4)])
+
+    model = PPO2(MlpLnLstmPolicy, Test_env, verbose=0, nminibatches=4)
+    model.learn(total_timesteps = 10000)
